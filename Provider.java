071bf774c6348fc6877f7ced7599f848f81e8230
kunj-arch <kunjbiharisrivastava08@gmail.com>
2024-08-23 01:56:12 +0530

    Add files via upload
    tools and technologies-
    IDE-eclipse
    DATA BASE-MySQL
    SERVER-apache tomcat 10
    
    JSP(JavaServer Pages): It is a technology that can enable the creation of dynamic web pages using Java and it can allow mixing the static HTML content with dynamic Java code. This file has .jsp extension and is translated into the servlets by the web container before being executed.
    Registration Form: The web form used to collect user information such as the name, phone number, address, etc. Typically for the registration process and these commonly on the websites and applications that are required the user accounts.
    Scriptlet: It is a block of the java code that can be embedded with a JSP page with the delimited by the <% %> tags.
    Request Parameter: The request parameter sent from the client to the server as part of the HTTP request. In the registration form, form inputs fields values are sent as the request parameters to the server when the form will be submitted.
    Server-side Processing: The processing of the data that can occur on the server after receiving the request from the client. The server-side processing involves tasks such as data validation database interaction and generating the response to send back to the client.

---
+++ Provider.java
@@ -0,0 +1,8 @@
+package bean;
+
+public interface Provider {
+	String DRIVER="com.mysql.cj.jdbc.Driver";  
+	String CONNECTION_URL="jdbc:mysql://localhost:3306/new";  
+	String USERNAME="root";  
+	String PASSWORD="KUNJ@$#@!"; 
+}
